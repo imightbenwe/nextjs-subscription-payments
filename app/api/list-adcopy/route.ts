4641f7f99282b37694d2032bf28a6f6cfe8bc93d
imightbenwe <koenw77@gmail.com>
2025-09-18 11:45:54 +0200

    Update route.ts

--- app/api/list-adcopy/route.ts
+++ app/api/list-adcopy/route.ts
@@ -5,7 +5,7 @@ export const dynamic = "force-dynamic";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! // needs insert/select rights
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 export async function GET() {
